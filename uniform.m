b974e89b7274bd521e9ead8f974cf46b397d1c5a
amirh4liyan <91153999+amirh4liyan@users.noreply.github.com>
2022-01-30 18:59:58 +0330

    correlation fixed

--- uniform.m
+++ uniform.m
@@ -15,18 +15,18 @@ pd = makedist('Uniform');
 cdf_var = cdf(pd, X);
 pdf_var = pdf(pd, X);
 
-% draw result for limited data (1 to 10)
-figure; plot(X, cdf_var); xlabel('data'); ylabel('cdf'); title('Section One | Uniform Distribution');
-figure; plot(X, pdf_var); xlabel('data'); ylabel('pdf'); title('Section One | Uniform Distribution');
+% draw cdf and pdf
+figure; plot(X, cdf_var); xlabel('data'); ylabel('cdf'); title('Uniform Distribution');
+figure; plot(X, pdf_var); xlabel('data'); ylabel('pdf'); title('Uniform Distribution');
 
 % histogram
-figure;hist(X);
+%figure;hist(X);
 
-% avg, var and covar
+% avg, var and corr
 average = mean(X);
 variance = var(X);
-covariance = cov(X);
+correlation = variance + (average).^2;
 
 disp(['average  : ' num2str(average)]);
 disp(['variance : ' num2str(variance)]);
-disp(['covariance :' num2str(covariance)]);
+disp(['correlation : ' num2str(correlation)]);
