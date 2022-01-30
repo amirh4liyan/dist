dd91fc238f482533c22cbb09f575955487e2823a
amirh4liyan <91153999+amirh4liyan@users.noreply.github.com>
2022-01-30 19:01:19 +0330

    corr fixed

--- normal.m
+++ normal.m
@@ -17,17 +17,17 @@ pd = makedist('Normal', 'mu', mu, 'sigma', sigma);
 cdf_var = cdf(pd, X);
 pdf_var = pdf(pd, X);
 
-% draw result for limited data (1 to 10)
-figure; plot(X, cdf_var); xlabel('data'); ylabel('cdf'); title('Section One | Normal Distribution ');
-figure; plot(X, pdf_var); xlabel('data'); ylabel('pdf'); title('Section One | Normal Distribution ');
+% draw cdf and pdf
+figure; plot(X, cdf_var); xlabel('data'); ylabel('cdf'); title('Normal Distribution');
+figure; plot(X, pdf_var); xlabel('data'); ylabel('pdf'); title('Normal Distribution');
 
 % histogram
-figure;hist(X);
+%figure;hist(X);
 
 % avg, var and covar
 average = mean(X);
 variance = var(X);
-correlation = corr(X);
+correlation = variance + (average).^2;
 
 disp(['average  : ' num2str(average)]);
 disp(['variance : ' num2str(variance)]);
