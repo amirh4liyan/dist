259043376f0cb451dac059aa4005b21f362871ac
arya <amirh4liyan@gmail.com>
2022-02-05 14:58:19 +0330

    more structive

--- normal.m
+++ normal.m
@@ -2,38 +2,38 @@
 % [+] 991771231
 
 
-% clear output
+% clear output and previous vars
 clc
 clear all
 
-% define N and x
+
+% define N, X and dist object
 N = 100000;
 X = randn(1, N);
-
 mu = 0;
 sigma = 1;
 pd = makedist('Normal', 'mu', mu, 'sigma', sigma);
 
-fun = @(x) ((1) ./ sqrt(sigma.*2.*pi)) .* exp(((-1) ./ (2)) .* ((x - mu) ./sigma));
-
+% compute pdf, cdf by theory
+func = @(x) (1 ./ sqrt(2.*pi)) .* exp((-1 ./ 2) .* (x.^2));
 PDFTHEORY = integral(fun, 0, 1);
 
-cdf_var = cdf(pd, X);
-pdf_var = pdf(pd, X);
+% compute pdf, cdf by software
+PDFSOFT = pdf(pd, X);
+CDFSOFT = cdf(pd, X);
 
 % draw cdf and pdf
-figure; plot(X, cdf_var); xlabel('data'); ylabel('cdf'); title('Normal Distribution');
-figure; plot(X, pdf_var); xlabel('data'); ylabel('pdf'); title('Normal Distribution');
+figure; plot(X, PDFSOFT); xlabel('data'); ylabel('software pdf'); title('Normal');
+figure; plot(X, CDFSOFT); xlabel('data'); ylabel('software cdf'); title('Normal');
 
 % histogram
 %figure;hist(X);
 
-% avg, var and covar
+% avg, var and corr
 average = mean(X);
 variance = var(X);
 correlation = variance + (average).^2;
 
-disp(['Theory  : ' num2str(PDFTHEORY)]);
 disp(['average  : ' num2str(average)]);
 disp(['variance : ' num2str(variance)]);
 disp(['correlation : ' num2str(correlation)]);
