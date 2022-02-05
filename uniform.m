d5793c3c533d317b026f111139ec3230f76625aa
arya <amirh4liyan@gmail.com>
2022-02-05 14:58:01 +0330

    Uniform finished :)

--- uniform.m
+++ uniform.m
@@ -6,30 +6,32 @@
 clc
 clear all
 
-% define N, X and dist object 
+
+% define range, N, X and dist object 
 a = 0;
 b = 1;
 N = 100000;
 X = rand(1, N);
 pd = makedist('Uniform');
 
+% compute pdf, cdf by theory
 PDFTHEORY = (b-a) ./ 1;
-PDFSOFT = pdf(pd, X);
 YPDF(1:N) = PDFTHEORY;
-
-
 CDFTHEORY_R1 = 0;
 CDFTHEORY_R2 = (X - a) ./ (b - a);
 CDFTHEORY_R3 = 1;
-CDFSOFT = cdf(pd, X);
 XX = [ 0 X 1 ];
 
-% draw cdf and pdf
-figure; plot(X, YPDF); xlabel('data'); ylabel('theoretical pdf'); title('Uniform Distribution');
-figure; plot(X, PDFSOFT); xlabel('data'); ylabel('software pdf'); title('Uniform Distribution');
+% compute pdf, cdf by software
+PDFSOFT = pdf(pd, X);
+CDFSOFT = cdf(pd, X);
 
-figure; plot(XX, [CDFTHEORY_R1 CDFTHEORY_R2 CDFTHEORY_R3]); xlabel('data'); ylabel('theoretical cdf'); title('Uniform Distribution');
-figure; plot(X, PDFSOFT); xlabel('data'); ylabel('software cdf'); title('Uniform Distribution');
+% draw cdf and pdf
+figure; plot(X, YPDF); xlabel('data'); ylabel('theoretical pdf'); title('Uniform');
+
+figure; plot(XX, [CDFTHEORY_R1 CDFTHEORY_R2 CDFTHEORY_R3]); xlabel('data'); ylabel('theoretical cdf'); title('Uniform');
+figure; plot(X, PDFSOFT); xlabel('data'); ylabel('software pdf'); title('Uniform');
+figure; plot(X, CDFSOFT); xlabel('data'); ylabel('software cdf'); title('Uniform');
 
 
 % histogram
@@ -43,5 +45,3 @@ correlation = variance + ((average).^2);
 disp(['average  : ' num2str(average)]);
 disp(['variance : ' num2str(variance)]);
 disp(['correlation : ' num2str(correlation)]);
-
-
