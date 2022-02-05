b2a9cdc888b6b5f07d8699ca5f9a2bbe1f0b8852
arya <amirh4liyan@gmail.com>
2022-02-05 21:54:32 +0330

    try for normal pdf ploting

--- normal.m
+++ normal.m
@@ -15,8 +15,9 @@ sigma = 1;
 pd = makedist('Normal', 'mu', mu, 'sigma', sigma);
 
 % compute pdf, cdf by theory
+range = -5:0.1:5;
 func = @(x) (1 ./ sqrt(2.*pi)) .* exp((-1 ./ 2) .* (x.^2));
-PDFTHEORY = integral(fun, 0, 1);
+PDFTHEORY = (1 ./ sqrt(2.*pi)) .* exp((-1 ./ 2) .* (range.^2));
 
 % compute pdf, cdf by software
 PDFSOFT = pdf(pd, X);
@@ -24,6 +25,7 @@ CDFSOFT = cdf(pd, X);
 
 % draw cdf and pdf
 figure; plot(X, PDFSOFT); xlabel('data'); ylabel('software pdf'); title('Normal');
+figure; plot(range, PDFTHEORY); xlabel('data'); ylabel('software pdf'); title('Normal');
 figure; plot(X, CDFSOFT); xlabel('data'); ylabel('software cdf'); title('Normal');
 
 % avg, var and corr
